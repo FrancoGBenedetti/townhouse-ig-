dfbfbdbcca0ae4cd483ab593971e4e4f2af590f9
Franco Benedetti <francobenedetti2@gmail.com>
2018-07-06 21:15:38 -0400

    correcciones

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
 
   get 'pages/numbers'
 
+
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
